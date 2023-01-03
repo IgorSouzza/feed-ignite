6854944b9133fed1c560d2343824c473aef15a40
Igor Souza <igor.souza.webmaster@gmail.com>
2023-01-03 11:47:45 -0300

    feat: add sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
 import './global.css'
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {/* Main content here */}
+        </main>
+      </div>
+    </div>
   )
 }
 
