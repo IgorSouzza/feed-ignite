0b3ea6f992d085f32c951df114307caf0ae72bf5
Igor Souza <igor.souza.webmaster@gmail.com>
2023-01-03 11:49:21 -0300

    fix: cover classname

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,7 +5,7 @@ import styles from './Sidebar.module.css'
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
         <img className={styles.avatar} src="https://github.com/igorSouzza.png" />
         <strong>Igor Souza</strong>
